99a10d565ee02024904a4fb9d37a12c80e55cc04
jorben12 <88739935+jorben12@users.noreply.github.com>
2025-12-03 19:45:07 +0100

    push met werken wrapper

--- Vivado_LED_toggling.ip_user_files/bd/basic_project/sim/basic_project.v
+++ Vivado_LED_toggling.ip_user_files/bd/basic_project/sim/basic_project.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2025.1 (win64) Build 6140274 Thu May 22 00:12:29 MDT 2025
-//Date        : Wed Dec  3 18:14:18 2025
+//Date        : Wed Dec  3 19:43:05 2025
 //Host        : Jorbenzijnpc running 64-bit major release  (build 9200)
 //Command     : generate_target basic_project.bd
 //Design      : basic_project
