7b37eee4354a458a972fe15c65a4eeed812a638e
Matteo_fey <66562258+warior456@users.noreply.github.com>
2025-12-04 10:52:10 +0100

    test123a

--- Vivado_LED_toggling.srcs/sources_1/new/FSM_basicProject.v
+++ Vivado_LED_toggling.srcs/sources_1/new/FSM_basicProject.v
@@ -1,28 +1,33 @@
 `timescale 1ns / 1ps
 
-module FSM_basicProject(
-        input wire iClk, iRst,
-        input wire iDown, iUp, iLeft, iRight,
+module FSM_basicProject
+
+
+    (
+        input wire iClk, iRst, iDown, iUp, iLeft, iRight,
         output wire [9 : 0] oShapeX, oShapeY, oShapSize,
         output wire oLEDUp, oLEDDown, oLEDLeft, oLEDRight
+
     );
-
-    // Registers for position
-    reg [9:0] rShapeX_current;
+    reg [9:0] rShapeX_current, wShapSize;
     reg [9:0] rShapeY_current;
-
-    localparam CLK_FREQ = 25000000;
-    // Timer wire
+    reg memX,memY;
+    localparam CLK_FREQ = 25_000_000;
     wire w_timer;
 
-    // 1. Timer Instantiation
-    // Generates a 1-cycle pulse every 0.05 seconds (50ms)
-    // Adjust SECONDS to make the square move faster or slower
-    timer_n_s#(.CLK_FREQ(CLK_FREQ),.SECONDS(0.05))
+    localparam sInit    = 3'b000;
+    localparam sIdle    = 3'b001;
+    localparam sUp      = 3'b010;
+    localparam sDown      = 3'b011;
+    localparam sRight      = 3'b100;
+    localparam sLeft      = 3'b101;
+    reg[2:0] rFSM_current, wFSM_next;
+
+
+    timer_n_s#(.CLK_FREQ(CLK_FREQ),.SECONDS(0.025))
              timer_50ms_inst(.iClk(iClk),.iRst(iRst),
                              .oQ(w_timer));
 
-    // 2. LED Logic (Existing logic kept as is)
     TIMER_LED_toggling_FSM timer_LED_toggling_FSM_up(
                                .iClk(iClk), .iRst(iRst), .iPush(iUp), .oLED(oLEDUp));
 
@@ -36,41 +41,105 @@ module FSM_basicProject(
                                .iClk(iClk), .iRst(iRst), .iPush(iRight), .oLED(oLEDRight));
 
 
-    // 3. Movement Logic
-    always @(posedge iClk) begin
-        if(iRst == 1) begin
-            // LAB REQUIREMENT: Reset to center (640/2 - 60/2 = 290)
+    always @(posedge  iClk) begin
+
+        rShapeX_current <= rShapeX_current;
+        rShapeY_current <= rShapeY_current;
+        rFSM_current <= wFSM_next;
+
+        // if (w_timer == 1) begin
+        //     memX <= 0;
+        //     memY <= 0;
+        // end
+        if(rFSM_current == sInit) begin
             rShapeX_current <= 10'd290;
             rShapeY_current <= 10'd210;
+            wShapSize <= 10'd60;
+            memX <= 0;
+            memY <= 0;
         end
-        else if (w_timer == 1) begin
-            // The timer outputs a pulse (1 tick) every 0.05s.
-            // We only update position when this pulse happens.
 
-            // --- Y AXIS MOVEMENT ---
-            if(iUp && rShapeY_current > 10'd0) begin
+        if(w_timer == 1) begin
+            if(rFSM_current == sUp && rShapeY_current != 10'd0 ) begin
                 rShapeY_current <= rShapeY_current - 10'd1;
-            end
-            else if(iDown && rShapeY_current < 10'd420) begin
-                // 480 (screen height) - 60 (shape size) = 420
-                rShapeY_current <= rShapeY_current + 10'd1;
+                memY <= 1;
             end
 
-            // --- X AXIS MOVEMENT ---
-            // Note: This is a separate IF block to allow diagonal movement
-            if(iLeft && rShapeX_current > 10'd0) begin
-                rShapeX_current <= rShapeX_current - 10'd1;
+            else if(rFSM_current == sDown && rShapeY_current != 10'd420 ) begin
+                rShapeY_current <= rShapeY_current + 10'd1;
+                memY <= 1;
             end
-            else if(iRight && rShapeX_current < 10'd580) begin
-                // 640 (screen width) - 60 (shape size) = 580
+
+            if(rFSM_current ==  sLeft && rShapeX_current != 10'd0  ) begin
+                rShapeX_current <= rShapeX_current - 10'd1;
+                memX <= 1;
+            end
+            else if(rFSM_current == sRight && rShapeX_current != 10'd580) begin
                 rShapeX_current <= rShapeX_current + 10'd1;
+                memX <= 1;
             end
         end
     end
 
-    // Output assignments
+    always @(*) begin
+        case (rFSM_current)
+            sInit:
+                wFSM_next <= sIdle;
+
+            sIdle:
+                if(iRst)
+                    wFSM_next <= sInit;
+                else if(iUp)
+                    wFSM_next<= sUp;
+                else if(iDown)
+                    wFSM_next<= sDown;
+                else if(iRight)
+                    wFSM_next<= sRight;
+                else if(iLeft)
+                    wFSM_next<= sLeft;
+                else
+                    wFSM_next <= sIdle;
+            sUp:
+                if(iRst)
+                    wFSM_next <= sInit;
+                else if(iUp)
+                    wFSM_next<= sUp;
+                else
+                    wFSM_next <= sIdle;
+
+            sDown:
+                if(iRst)
+                    wFSM_next <= sInit;
+                else if(iDown)
+                    wFSM_next<= sDown;
+                else
+                    wFSM_next <= sIdle;
+
+            sLeft:
+                if(iRst)
+                    wFSM_next <= sInit;
+                else if(iLeft)
+                    wFSM_next<= sLeft;
+                else
+                    wFSM_next <= sIdle;
+
+            sRight:
+                if(iRst)
+                    wFSM_next <= sInit;
+                else if(iRight)
+                    wFSM_next<= sRight;
+                else
+                    wFSM_next <= sIdle;
+
+
+            default:
+                wFSM_next <= sIdle;
+        endcase
+    end
+
+
     assign oShapeX = rShapeX_current;
     assign oShapeY = rShapeY_current;
-    assign oShapSize = 10'd60; // Fixed size as per lab
+    assign oShapSize = wShapSize;
 
 endmodule
